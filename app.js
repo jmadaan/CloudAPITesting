c158cb42f567d67220074dfd0644d18eafca119f
Jeetan Madaan <madajeeblog@gmail.com>
2019-01-11 19:26:09 -0500

    cloudapitesting:c5

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var app = express();
 var request = require('request');
-var port = process.env.PORT || 3000;
-
+var app = express();
 var port = process.env.PORT || 3000;
 
 
@@ -15,52 +13,14 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('home');
+  let message = "I2Chain API Testing Stack";
+  res.render('home', {serverMessage: message});
 });
 
-app.get('/register', function(req, res) {
-	
-	console.log("Test Register API Call");
-	/*request('http://www.google.com', function (error, response, body) {
-	  console.log('error:', error); // Print the error if one occurred
-	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	  //console.log('body:', body); // Print the HTML for the Google homepage.
-	});*/
-
-	let email = "test@i2chain.com";
-	let password = "password123";
-	let pubKey = "pubkey123";
-	const params = {
-      email,
-      password,
-      pubKey
-    };
-	request.post(
-      {
-        uri: `http://${restApiUrl}:${restApiPort}/api/user/register/`,
-        body: params,
-        json: true
-      },
-      (error, response, body) => {
-        //console.log('Sent register:\n', params, body);
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //console.log('body:', JSON.stringify(body));
-      }
-    );
-
-	
-});
 app.get('/getall', function(req, res) {
-	
-	console.log("Test getAll API Call");
-	/*request('http://www.google.com', function (error, response, body) {
-	  console.log('error:', error); // Print the error if one occurred
-	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	  //console.log('body:', body); // Print the HTML for the Google homepage.
-	});*/
-
-	request.get(
+  
+  console.log("Test getAll API Call");
+  request.get(
       {
         uri: `http://${restApiUrl}:${restApiPort}/api/user/getAll/`,
         headers: {
@@ -82,12 +42,68 @@ app.get('/getall', function(req, res) {
     );
 });
 
+app.get('/login', function(req, res) {
+  console.log("Test Login API Call");
+  let message = "Login API Test";
+  let email = "test1@i2chain.com";
+  let password = "password123";
+  const params = {
+      email,
+      password
+    };
+  request.post(
+      {
+        uri: `http://${restApiUrl}:${restApiPort}/api/user/login/`,
+        body: params,
+        json: true
+      },
+      (error, response, body) => {
+        //console.log('Sent register:\n', params, body);
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        let message = "Login API Test";
+        res.render('login', {serverMessage: message});
+        console.log('body:', JSON.stringify(body));
+      }
+    );
+});
 
+app.get('/register', function(req, res) {
+	console.log("Test Register API Call");
+	/*request('http://www.google.com', function (error, response, body) {
+	  console.log('error:', error); // Print the error if one occurred
+	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  //console.log('body:', body); // Print the HTML for the Google homepage.
+	});*/
+	let email = "test1@i2chain.com";
+	let password = "password123";
+	let pubKey = "pubkey123";
+	const params = {
+      email,
+      password,
+      pubKey
+    };
+	request.post(
+      {
+        uri: `http://${restApiUrl}:${restApiPort}/api/user/register/`,
+        body: params,
+        json: true
+      },
+      (error, response, body) => {
+        //console.log('Sent register:\n', params, body);
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        let message = "Register API Test";
+        res.render('register', {serverMessage: message});
+        console.log('body:', JSON.stringify(body));
+      }
+    );	
+});
 
 app.get('/ejstest', function(req, res) {
 	
 	let message = "View Engine is EJS";
-	res.render('index', {serverMessage: message});
+	res.render('home', {serverMessage: message});
 	
 });
 
